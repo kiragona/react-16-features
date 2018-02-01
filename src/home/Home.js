ab9ba644ba08168123e3cfb45bba78868f2b721a
marianna <marianna.etelson@nielsen.com>
2018-02-01 17:54:48 +0200

    revert test commit

--- src/home/Home.js
+++ src/home/Home.js
@@ -9,7 +9,7 @@ const Home = () => (
   <div className='home-content'>
     <div className="home">
       <img src={logo} className="app-logo" alt="logo"/>
-      <div className="app-title">React 16 feature demo !!!</div>
+      <div className="app-title">React 16 feature demo</div>
     </div>
   </div>
 

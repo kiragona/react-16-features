b52b509d60a7571207826fb4d218f8b5ccfa93d7
marianna <marianna.etelson@nielsen.com>
2018-03-19 18:27:18 +0200

    new 16.3.0 feats

--- src/breakingChanges/BreakingChanges.js
+++ src/breakingChanges/BreakingChanges.js
@@ -2,12 +2,14 @@ import React, {Component, Fragment} from 'react'
 
 import 'prismjs/themes/prism.css'
 
+import strictError from '../images/strictError.png'
+
 
 export default class BreakingChanges extends Component {
 
   constructor(props) {
     super(props)
-    this.exampleCount = 10
+    this.exampleCount = 12
     this.curExampleNum = 1
     this.state = {
       exampleNumber: 1,
@@ -54,20 +56,23 @@ export default class BreakingChanges extends Component {
 
 
         <div style={{overflow: 'hidden', overflowY: 'auto'}} className='example'>
-          {(this.state.exampleNumber <= this.exampleCount  ) &&
+          {(this.state.exampleNumber <= this.exampleCount) &&
           <div>
             <div className='example-title'>Breaking Changes and Deprecations</div>
             <br/>
             <div className='example-note'>&emsp;<span>✔</span>️&emsp; <b>Upgrading</b></div>
 
-            <div className='example-note example-note-tab'><span>•</span> &emsp; In terms of upgrading, you can think of this like any other major React release.
+            <div className='example-note example-note-tab'><span>•</span> &emsp; In terms of upgrading, you can think of
+              this like any other major React release.
             </div>
-            <br/>
-            <div className='example-note example-note-tab'><span>•</span> &emsp; With minor exceptions, if your app runs in 15.6 <b>without any warnings</b>, it
+
+            <div className='example-note example-note-tab'><span>•</span> &emsp; With minor exceptions, if your app runs
+              in 15.6 <b>without any warnings</b>, it
               should work in 16.
             </div>
-            <br/>
-            <div className='example-note example-note-tab'><span>•</span> &emsp; Codemods (check react-codemod project) are provided to automatically transform
+
+            <div className='example-note example-note-tab'><span>•</span> &emsp; Codemods (check react-codemod project)
+              are provided to automatically transform
               your deprecated code.
             </div>
           </div>
@@ -75,55 +80,92 @@ export default class BreakingChanges extends Component {
 
           <div>
             <br/>
-            {(this.state.exampleNumber >= 2 ) &&
+            {(this.state.exampleNumber >= 2) &&
             <div className='example-note'>&emsp;<span>✔</span>️️&emsp; <b>Deprecations</b></div>
             }
             {(this.state.exampleNumber >= 2) &&
-            <div className='example-note example-note-tab'><span>•</span> &emsp; use <b>ReactDOM.hydrate</b> instead of <b>ReactDOM.render</b> for server side
+            <div className='example-note example-note-tab'><span>•</span> &emsp; Use <b>ReactDOM.hydrate</b> instead
+              of <b>ReactDOM.render</b> for server side
               rendering</div>
             }
-            <br/>
-            {(this.state.exampleNumber >= 3 ) &&
-            <div className='example-note example-note-tab'><span>•</span> &emsp; Keep using ReactDOM.render if you’re just doing client-side rendering.
+            {(this.state.exampleNumber >= 3) &&
+            <div className='example-note example-note-tab'><span>•</span> &emsp; Keep using <b>ReactDOM.render</b> if you’re
+              just doing client-side rendering.
             </div>
             }
-            <br/>
-            {(this.state.exampleNumber >= 4 ) &&
+            {(this.state.exampleNumber >= 4) &&
             <div className='example-note example-note-tab'><span>•</span> &emsp; <b>React.PropTypes</b>:
               use PropTypes from <b>prop-types</b> package, not from React object</div>
-            }<br/>
+            }
             {(this.state.exampleNumber >= 5) &&
             <div className='example-note example-note-tab'><span>•</span> &emsp; <b>React.createClass</b>:
               use <b>createReactClass</b> from <b>create-react-class</b> package, not from React object</div>
-            }<br/>
+            }
             {(this.state.exampleNumber >= 6) &&
-            <div className='example-note example-note-tab'><span>•</span> &emsp; Discontinuing support for React Addons: <a
-              href="https://reactjs.org/blog/2017/04/07/react-v15.5.0.html#discontinuing-support-for-react-addons" rel="noopener noreferrer" target="_blank"> <b>more details</b></a>
+            <div className='example-note example-note-tab'><span>•</span> &emsp; Discontinuing support for React
+              Addons: <a
+                href="https://reactjs.org/blog/2017/04/07/react-v15.5.0.html#discontinuing-support-for-react-addons"
+                rel="noopener noreferrer" target="_blank"> <b>more details</b></a>
             </div>
             }<br/>
 
             {(this.state.exampleNumber >= 7) &&
-            <div className='example-note'>&emsp;<span>✔</span>️️&emsp; <b>Scheduling and lifecycle methods changes</b></div>
-            }<br/>
+            <div className='example-note'>&emsp;<span>✔</span>️️&emsp; <b>Scheduling and lifecycle methods changes</b>
+            </div>
+            }
 
             {(this.state.exampleNumber >= 8) &&
             <div>
-              <div className='example-note example-note-tab'><span>•</span> &emsp; ReactDOM.render() and ReactDOM.unstable_renderIntoContainer() now return null
+              <div className='example-note example-note-tab'><span>•</span> &emsp; ReactDOM.render() and
+                ReactDOM.unstable_renderIntoContainer() now return null
               </div>
               <div className='example-note example-note-double-tab'>if called from inside a lifecycle method</div>
-              <div className='example-note example-note-double-tab'>• &emsp; To work around this, you can either use the <b>new portal</b> API
+              <div className='example-note example-note-double-tab'>• &emsp; To work around this, you can either use
+                the <b>new portal</b> API
               </div>
             </div>
             }<br/>
             {(this.state.exampleNumber >= 9) &&
-            <div className='example-note example-note-tab'>• &emsp;&emsp; New lifecycle method <b>ComponentDidCatch(error, info)</b> API
+            <div className='example-note example-note-tab'>• &emsp;&emsp; New lifecycle method <b>ComponentDidCatch(error,
+              info)</b> API
+            </div>
+            }
+
+            {(this.state.exampleNumber >= 10) &&
+            <div>
+              <div className='example-note example-note-tab'>• &emsp;&emsp; 16.3.0 - Depricated <b>componentWillMount</b>,
+                use <b>componentDidMount</b>
+              </div>
+              <div className='example-note example-note-tab'>• &emsp;&emsp; 16.3.0 - Depricated <b>componentWillUpdate</b>,
+                use <b>componentDidUpdate</b>
+              </div>
+              <div className='example-note example-note-tab'>• &emsp;&emsp; 16.3.0 - Depricated <b>componentWillReceiveProps</b>,
+                use a new static function <b>getDerivedStateFromProps</b>
+              </div>
             </div>
             }<br/>
 
-            {(this.state.exampleNumber >= 10 ) &&
-            <div className='example-note'>&emsp;<span>✔</span>️️&emsp; <b><a rel="noopener noreferrer" href="https://reactjs.org/blog/2017/09/26/react-v16.0.html#breaking-changes"
-                                                               target="_blank">...and more</a></b></div>
+            {(this.state.exampleNumber >= 11) &&
+            <div className='example-note'>&emsp;<span>✔</span>️️&emsp; <b>16.3.0 - New Components</b>
+            </div>
+            }<br/>
+
+            {(this.state.exampleNumber >= 12) &&
+            <div>
+              <div className='example-note example-note-tab'>• &emsp;&emsp; <b>StrictMode</b>, a new way to make sure
+                your code is following the best practices
+              </div>
+              <div className='example-note example-note-double-tab'>• &emsp; Available under <b>React.StrictMode</b> and can be
+                added to your app tree or sub-tree
+              </div>
+              <div className='example-note example-note-double-tab'>• &emsp; If one of the children components, that
+                wrapped by StrictMode component, uses some of the method mentioned above, an error message will appear
+                in browser console:
+              </div>
+              <img alt='StrictMode Error' className='strict-warning' src={strictError}/>
+            </div>
             }
+
 
           </div>
 
